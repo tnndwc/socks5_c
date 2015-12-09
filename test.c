d7c1f4e88fe37a1f9c9b0dfb982d7d19f22b6bb2
Len <tnndwc@gmail.com>
2015-12-09 23:16:03 +0800

    learn-15.12.9.1

---
+++ test.c
@@ -0,0 +1,10 @@
+//
+// Created by Len on 12/9/15.
+//
+
+#include "test.h"
+#include <stdio.h>
+
+int main(){
+    printf("Hello CLion");
+}
---
+++ test.h
@@ -0,0 +1,8 @@
+//
+// Created by Len on 12/9/15.
+//
+
+#ifndef SOCKS5_C_TEST_H
+#define SOCKS5_C_TEST_H
+
+#endif //SOCKS5_C_TEST_H
