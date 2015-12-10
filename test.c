02c137c732fc95fd0887f5061278ec28701fb809
Alex <alexemail7@126.com>
2015-12-10 18:57:06 +0800

    learn-15.12.10.1

--- test.c
+++ test.c
@@ -4,7 +4,28 @@
 
 #include "test.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+struct encry_key_s {
+    size_t len;
+    uint8_t key[0];
+};
 
 int main(){
-    printf("Hello CLion");
+    printf("Hello CLion\n");
+
+    int i = atoi("10");
+    printf("atoi is %d\n", i);
+
+    struct encry_key_s *key = NULL;
+    size_t key_len;
+    char st[] = "HelloTest";
+    key_len = strlen(st);
+    key = malloc(sizeof(*key) + key_len);
+    key->len = key_len;
+    memcpy(key->key, st, key_len);
+
+    printf(&key->key);
 }
